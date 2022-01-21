5cc9d43d9af7b69080ecceef14d5912e88ec8f78
MICHAEL SANTOS DE OLIVEIRA <michaelsoliveira@gmail.com>
2022-01-21 19:30:13 -0300

    Implementação do Cadastro de Empresa

--- services/axios.ts
+++ services/axios.ts
@@ -2,8 +2,10 @@ import axios from 'axios'
 import { NextApiRequest, NextPageContext } from 'next'
 import { parseCookies } from 'nookies'
 import * as cookie from 'cookie';
+import { useRouter } from 'next/router'
 
 export function apiClient(ctx?: any) {
+    
     const { 'x-access-token': token } = parseCookies(ctx)
 
     const api = axios.create({
@@ -12,10 +14,18 @@ export function apiClient(ctx?: any) {
     })
 
     api.interceptors.request.use(config => {
+        
 
         return Promise.resolve(config)
+    }, error => {
+        const router = useRouter()
+        if (error.response.status === 401) {
+            router.push('/unauthorized')
+        }
+        
+        return Promise.reject(error)
     })
-
+    
     if (token) {
         api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     }
